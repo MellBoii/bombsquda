58fae76df39fa03b97cfeb5011709311ddf00656
Mell <63241791+MellBoii1@users.noreply.github.com>
2026-05-08 10:10:27 -0300

    new intro

--- ba_data/python/bascenev1lib/intros/main.py
+++ ba_data/python/bascenev1lib/intros/main.py
@@ -142,3 +142,98 @@ class SegaIntro(Intro):
         bs.timer(4, animate4)
         bs.timer(5, animate5)
 
+@register
+class MarioMakerIntro(Intro):
+    def __init__(self):
+        import fromgoverhaul.mell_resources as mell
+        import random
+        super().__init__()
+        bs.getsound('slideWhistleReverse')
+        bs.getsound('slideWhistle')
+        bs.getsound('mortal_damage')
+        bs.getsound('ring_spill')
+        bs.getsound('cheer2')
+        bs.getsound('smashDied')
+        self.scream = bs.getsound(random.choice(mell.screams))
+    @override
+    def start(self):
+        from bascenev1lib.actor.nodejumper import ImageJumper
+        import fromgoverhaul.mell_resources as mell
+        self.bg = bs.newnode(
+            'image',
+            delegate=self,
+            attrs={
+                'fill_screen': True,
+                'texture': bs.gettexture('white2'),
+            },
+        )
+        sc = 1.4
+        logo_image = bs.newnode('image', 
+            attrs={
+                'texture': bs.gettexture('mell_logo'),
+                'position': (0, 0), 
+                'scale': (512 * sc, 256 * sc),
+                'opacity': 1.0,
+            }
+        )
+        hsc = 1.4
+        hand = bs.newnode('image', 
+            attrs={
+                'texture': bs.gettexture(f'{PATH}spaz_mm_hand'),
+                'position': (5000, 0), 
+                'scale': (256 * hsc, 512 * hsc),
+                'opacity': 1.0,
+            }
+        )
+        def end_it():
+            bs.getsound('smashDied').play()
+            bs.getsound('cheer2').play()
+            bs.timer(3, self.stop)
+        def jump_logo():
+            ImageJumper.jump_to_position(
+                logo_image,
+                target_pos=(4000, 0),
+                time=3,
+            )
+            bs.getsound('mortal_damage').play()
+            bs.getsound('ring_spill').play()
+            self.scream.play()
+            bs.timer(1, end_it)
+        def animatehand():
+            xoffs = 500
+            yoffs = 500
+            animate_array = {
+                0: (579 - xoffs, -260 - yoffs),
+                0.71: (579 - xoffs, 121 - yoffs),
+                0.85: (607 - xoffs, 247 - yoffs),
+                0.92: (527 - xoffs, 254 - yoffs),
+                1.02: (445 - xoffs, 260 - yoffs),
+                1.13: (368 - xoffs, 251 - yoffs),
+                1.23: (320 - xoffs, 245 - yoffs),
+                1.34: (288 - xoffs, 236 - yoffs),
+                1.59: (282 - xoffs, 233 - yoffs),
+                1.7: (289 - xoffs, 229 - yoffs),
+                1.81: (315 - xoffs, 223 - yoffs),
+                1.91: (402 - xoffs, 214 - yoffs),
+                2.02: (454 - xoffs, 209 - yoffs),
+                2.12: (477 - xoffs, 201 - yoffs),
+                2.31: (477 - xoffs, 201 - yoffs),
+                2.42: (419 - xoffs, 177 - yoffs),
+                2.54: (400 - xoffs, 168 - yoffs),
+                2.72: (399 - xoffs, 168 - yoffs),
+                3.28: (520 - xoffs, 166 - yoffs),
+                3.35: (800 - xoffs, 300 - yoffs),
+                4: (800 - xoffs, 300 - yoffs),
+                5: (800 - xoffs, -500 - yoffs),
+            }
+            bs.getsound('slideWhistle').play()
+            bs.timer(4, bs.getsound('slideWhistleReverse').play)
+            bs.animate_array(
+                hand, 
+                'position', 
+                2, 
+                animate_array
+            )
+            bs.timer(3.3, jump_logo)
+        bs.timer(3, animatehand)
+
